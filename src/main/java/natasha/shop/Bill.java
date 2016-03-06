7950d8aed1fd3f47e89dbb2f5ff1680b7edf5655
NataliyaD-in6k <NataliyaD@softwareplanet.uk.com>
2016-03-06 11:37:01 +0200

    Class Bill added

--- src/main/java/natasha/shop/Bill.java
+++ src/main/java/natasha/shop/Bill.java
@@ -1,5 +1,34 @@
 package natasha.shop;
 
-public class Bill {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Bill {
+    private ShoppingCart shoppingCart;
+    private List<String> topMessages = new ArrayList<String>();
+    private List<String> bottomMessages = new ArrayList<String>();
+
+    public Bill(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+
+    public void addMessageToTop(String message){
+        topMessages.add(message);
+    }
+
+    public void addMessageToBottom(String message){
+        bottomMessages.add(message);
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public List<String> getTopMessages() {
+        return topMessages;
+    }
+
+    public List<String> getBottomMessages() {
+        return bottomMessages;
+    }
 }
